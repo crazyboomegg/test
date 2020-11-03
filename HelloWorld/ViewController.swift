c565a17f323144b164203f1c40a8a4440b01d267
crazyboomegg <crazyboomegg@gmail.com>
2020-11-03 11:34:23 +0800

    test

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 江柏毅 on 2020/8/20.
+//  Copyright © 2020 Boy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func showMessage(sender: UIButton){
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
